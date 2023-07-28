d04043388dac260874333354f1a46cec740ac335
Iman Akbar <iman.akbar@edi-indonesia.co.id>
2023-07-28 07:07:25 +0700

    post method

--- test5/test/demo/src/main/java/com/example/demo/controller/UserController.java
+++ test5/test/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,29 +1,37 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.PaymentDto;
 import com.example.demo.model.PaymentModel;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedHashMap;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/qr")
 public class UserController {
 
     @Autowired
     UserRepository userRepository;
 
     @GetMapping(path = "/getAll", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity getAllUser(){
+    public ResponseEntity getAllUser() {
         LinkedHashMap<String, Object> res = new LinkedHashMap<>();
         List<PaymentModel> data = userRepository.getPayment();
         res.put("data", data);
+        return ResponseEntity.ok().body(res);
+
+    }
+
+    @PostMapping(path = "/payment", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<?> postPayment(@RequestBody PaymentDto paymentDto) {
+        LinkedHashMap<String, Object> res = new LinkedHashMap<>();
+        userRepository.postPayment(paymentDto.getProcessingode(), paymentDto.getAdditionalfield());
+        res.put("data", "ok");
         return ResponseEntity.ok().body(res);
     }
 }
--- test5/test/demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ test5/test/demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.model.PaymentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -13,4 +16,8 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<PaymentModel,String> {
     @Query(value = "select * from Payment" , nativeQuery = true)
     List<PaymentModel> getPayment();
+
+    @Modifying
+    @Query(value = "insert into payment (processingCode, additionalField) values(:processingode, :additionalfield)" , nativeQuery = true)
+    public void postPayment(@Param("processingode") String processingode, @Param("additionalfield") String additionalfield);
 }
